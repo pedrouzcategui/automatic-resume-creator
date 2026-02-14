8751a4019b50c88e067ff0966b76ef6aa9e79c4a
Pedro <pedrouzcategui0103@gmail.com>
2026-02-14 19:58:50 -0400

    Add Test for ChatGPT Service

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
-
 from api.chatgpt import ChatGPTService
-    
+
 def main():
-    response = ChatGPTService.generate(prompt="Return the name of the first three starter pokemons in the first pokemon game", model="gpt-5.0")
+    response = ChatGPTService.generate(prompt="Return the name of the first three starter pokemons")
     print(response)
 
 if __name__ == "__main__":
