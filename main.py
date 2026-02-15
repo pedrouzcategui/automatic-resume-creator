8b8480f1235f4f07298d4fad163d892ed6722615
Pedro <pedrouzcategui0103@gmail.com>
2026-02-14 20:41:25 -0400

    Fixes function name call

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from utils.job_post import extract_job_post_details
+from utils.job_post import extract_job_details_from_job_post_url
 
 def main():
     url = input("Enter the URL of the job posting: ")
-    details = extract_job_post_details(url)
+    details = extract_job_details_from_job_post_url(url)
     print("Extracted Job Post Details:")
     for key, value in details.items():
         print(f"{key}: {value}")
